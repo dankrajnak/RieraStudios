b56da2f96268e0bc7f02a6b93b915da1f73fe35c
dankrajnak <dan.krajnak@gmail.com>
2016-09-19 20:05:03 -0400

    Eliminated map in config

--- config.php
+++ config.php
@@ -7,19 +7,4 @@ define("ROOT_URL", "http://localhost/Riera/");
 //Define assets I guess
 define("STANDARD_CSS", "/Riera/assets/css/stylesheet.css");
 
-//Define artist maps
-global $ARTBRUT_MAP;
-$ARTBRUT_MAP = array(
-	"Alejandro" => "Alejandro Valdes Matute",
-	"Boris" => "Boris Adolfo Martin SantaMaria",
-	"Carlos" => "Carlos J. Garcia Huergo",
-	"Luis" => "Luis de Jesus Sotorrios Fabregas",
-	"Coto" => "Luis Eusebio Coto",
-	"Daldo" => "Daldo Marte",
-	"Damian" => "Damian Valdes Dilla",
-	"Darien" => "Darien Robaina Alonso",
-	"Gallo" => "Hector Gallo Portieles",
-	"Guillermo" => "Guillermo Rigoberto Casola Marcos",
-	);
-
 ?>
