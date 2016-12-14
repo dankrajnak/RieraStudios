407b8c39e498e1f6c71ba9b84471d82320c72bdd
Daniel Krajnak <dan.krajnak@gmail.com>
2016-12-14 00:56:32 -0500

    Created responsive standard navigation (unstyled) and removed .DS_Store from git

--- standardElements/standardHeader.php
+++ standardElements/standardHeader.php
@@ -3,6 +3,9 @@
 <head>
 	<meta charset="ISO-8859-1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH;?>assets/css/bootstrap.css" />
 	<link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH.STANDARD_CSS;?>"/>
+	<script src="<?php echo ROOT_PATH;?>assets/js/libraries/jquery.js"></script>
+	<script src="<?php echo ROOT_PATH;?>assets/js/bootstrap.js"></script>
 	<title>Riera Studio - Havana, Cuba </title>
 </head>
--- standardElements/standardNavBar.php
+++ standardElements/standardNavBar.php
@@ -1,18 +1,34 @@
-<div class="menu">
-	<div id="menu-title"><a href="<?php echo ROOT_PATH ?>">RIERA STUDIO</a></div>
-	<ul>
-		<li><a href="<?php echo ROOT_PATH ?>about/">ABOUT US</a></li>
-		<li><a href="<?php echo ROOT_PATH ?>artbrut/">ART BRUT PROJECT CUBA</a></li>
-		<li class = "dropdown" id="nav-dropdown">
-				ARTISTS
-				<div class="dropdown-content" id="nav-dropdown-content">
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/">ART BRUT</a>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/">OUTSIDER ART</a>
-				</div>
-		</li>
-		<li><a href="<?php echo ROOT_PATH ?>exhibitions/">EXHIBITIONS</a></li>
-		<li><a href="http://rierastudioart.cubava.cu">BLOG</a></li>
-		<li><a href="<?php echo ROOT_PATH ?>press/">PRESS RELEASE</a></li>
-		<li><a href="<?php echo ROOT_PATH ?>contact/">CONTACT</a></li>
-	</ul>
-</div>
+<nav class = "navbar navbar-default navbar-fixed-top">
+	<div class="containter-fluid">
+		<div class ="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">MENU
+			<span class="sr-only">Toggle navigation</span>
+			</button>
+			<a id="menu-title" class= "navbar-brand" href="<?php echo ROOT_PATH ?>">RIERA STUDIO</a>
+		</div>
+
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class= "nav navbar-nav">
+				<li class="dropdown">
+          			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">ABOUT US
+          			</a>
+					<ul class = "dropdown-menu">
+						<li><a href="<?php echo ROOT_PATH ?>/about/">OUR STUDIO</a></li>
+						<li><a href="<?php echo ROOT_PATH ?>/artbrut/">ART BRUT PROJECT CUBA</a></li>
+					</ul>
+				</li>
+				<li class = "dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">ARTISTS</a>
+					<ul class = "dropdown-menu">
+						<li><a href="<?php echo ROOT_PATH ?>artbrutartists/">ART BRUT</a></li>
+						<li><a href="<?php echo ROOT_PATH ?>outsiderartartists/">OUTSIDER ART</a></li>
+					</ul>
+				</li>
+				<li><a href="<?php echo ROOT_PATH ?>exhibitions/">EXHIBITIONS</a></li>
+				<li><a href="http://rierastudioart.cubava.cu">BLOG</a></li>
+				<li><a href="<?php echo ROOT_PATH ?>press/">PRESS RELEASE</a></li>
+				<li><a href="<?php echo ROOT_PATH ?>contact/">CONTACT</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
