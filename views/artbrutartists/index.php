feaa1e33c7d4decead85472e2833e40e96398774
Daniel Krajnak <dan.krajnak@gmail.com>
2016-12-18 03:06:34 -0500

    All artist pages and tables repsonsive

--- views/artbrutartists/index.php
+++ views/artbrutartists/index.php
@@ -3,213 +3,144 @@
 		<div class="sectiontitle" id="artbrutartistssectiontitle" style="text-align: center; padding-top:40px;">
 			ART BRUT ARTISTS
 		</div>
-		<table class="sectiontablecontent" id="artbrutartistssectiontablecontent">
-			<tbody>
-			<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Alejandro-Valdes-Matute/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/alejandroartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/alejandroartistred.jpg"></image>
-					<span>ALEJANDRO VALDES MATUTE</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Boris-Adolfo-Martin-Santamaria/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/borisartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/borisartistred.jpg"></image>
-					<span>BORIS ADOLFO MARTIN SANTAMARIA</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Carlos-J-Garcia-Huergo/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/carlitosartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/carlitosartistred.jpg"></image>
-					<span>CARLOS J. GARCIA HUERGO</span>
-					</a>
-				</td>
-			</tr>
+		<div class="container artisttable">
+			<div class= "col-sm-4 col-md-4 col-lg-4">	
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Alejandro-Valdes-Matute/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/alejandroartist.jpg"/>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/alejandroartistred.jpg"/>
+				<h4>ALEJANDRO VALDES MATUTE</h4>
+				</a>
+			</div>
+			
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Boris-Adolfo-Martin-Santamaria/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/borisartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/borisartistred.jpg"></image>
+				<h4>BORIS ADOLFO MARTIN SANTAMARIA</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Carlos-J-Garcia-Huergo/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/carlitosartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/carlitosartistred.jpg"></image>
+				<h4>CARLOS J. GARCIA HUERGO</h4>
+				</a>
+			</div>
 
-					<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Luis-De-Jesus-Sotorrios-Fabregas/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/chuchoartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/chuchoartistred.jpg"></image>
-					<span>LUIS DE JESUS SOTORRIOS FABREGAS</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Luis-Eusebio-Coto/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/cotoartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/cotoartistred.jpg"></image>
-					<span>LUIS EUSEBIO COTO</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Daldo-Marte/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/daldoartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/daldoartistred.jpg"></image>
-					<span>DALDO MARTE</span>
-					</a>
-				</td>
-			</tr>
-					<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Damian-Valdes-Dilla/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/damianartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/damianartistred.jpg"></image>
-					<span>DAMIAN VALDES DILLA</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Darien-Robaina-Alonso/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/darienartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/darienartistred.jpg"></image>
-					<span>DARIEN ROBAINA ALONSO</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Josvedy-Jove-Junco/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/elsirioartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/elsirioartistred.jpg"></image>
-					<span>JOSVEDY JOVE JUNCO "EL SIRIO"</span>
-					</a>
-				</td>
-			</tr>
-					<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Hector-Gallo-Portieles/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/galloartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/galloartistred.jpg"></image>
-					<span>HECTOR GALLO PORTIELES</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Hector-Gonzales-Herrero/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/hectorartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/hectorartistred.jpg"></image>
-					<span>HECTOR GONZALES HERRERO</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Jorge-Casola-Marcos/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/kekeartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/kekeartistred.jpg"></image>
-					<span>JORGE CASOLA MARCOS "KEKE"</span>
-					</a>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Lien-Agrelo-Pons/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/lienartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/lienartistred.jpg"></image>
-					<span>LIEN AGRELO PONS</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Misleidys-De-La-Caridad-Castillo-Pedroso/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/misleidysartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/misleidysartistred.jpg"></image>
-					<span>MISLEIDYS DE LA CARIDAD CASTILLO PEDROSO</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Miguel-Ramon-Morales-Diaz/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/ramonartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/ramonartistred.jpg"></image>
-					<span>MIGUEL RAMON MORALES DIAZ</span>
-					</a>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Guillermo-Rigoberto-Casola-Marcos/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/rigoartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/rigoartistred.jpg"></image>
-					<span>GUILLERMO RIGOBERTO CASOLA MARCOS</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>artbrutartists/Roberto-OFarrill-Multan/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/robertoartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/robertoartistred.jpg"></image>
-					<span>ROBERTO O'FARRILL MULTAN</span>
-					</a>
-				</td>
-				<td>
-				</td>
-			</tr>
-			</tbody>
-		</table>
+			<div class="clearfix "></div>
+
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Luis-De-Jesus-Sotorrios-Fabregas/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/chuchoartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/chuchoartistred.jpg"></image>
+				<h4>LUIS DE JESUS SOTORRIOS FABREGAS</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Luis-Eusebio-Coto/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/cotoartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/cotoartistred.jpg"></image>
+				<h4>LUIS EUSEBIO COTO</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Daldo-Marte/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/daldoartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/daldoartistred.jpg"></image>
+				<h4>DALDO MARTE</h4>
+				</a>
+			</div>
+
+			<div class="clearfix "></div>
+
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Damian-Valdes-Dilla/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/damianartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/damianartistred.jpg"></image>
+				<h4>DAMIAN VALDES DILLA</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Darien-Robaina-Alonso/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/darienartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/darienartistred.jpg"></image>
+				<h4>DARIEN ROBAINA ALONSO</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Josvedy-Jove-Junco/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/elsirioartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/elsirioartistred.jpg"></image>
+				<h4>JOSVEDY JOVE JUNCO "EL SIRIO"</h4>
+				</a>
+			</div>
+
+			<div class="clearfix "></div>
+
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Hector-Gallo-Portieles/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/galloartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/galloartistred.jpg"></image>
+				<h4>HECTOR GALLO PORTIELES</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Hector-Gonzales-Herrero/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/hectorartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/hectorartistred.jpg"></image>
+				<h4>HECTOR GONZALES HERRERO</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Jorge-Casola-Marcos/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/kekeartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/kekeartistred.jpg"></image>
+				<h4>JORGE CASOLA MARCOS "KEKE"</h4>
+				</a>
+			</div>
+
+			<div class="clearfix "></div>
+
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Lien-Agrelo-Pons/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/lienartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/lienartistred.jpg"></image>
+				<h4>LIEN AGRELO PONS</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Misleidys-De-La-Caridad-Castillo-Pedroso/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/misleidysartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/misleidysartistred.jpg"></image>
+				<h4>MISLEIDYS DE LA CARIDAD CASTILLO PEDROSO</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Miguel-Ramon-Morales-Diaz/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/ramonartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/ramonartistred.jpg"></image>
+				<h4>MIGUEL RAMON MORALES DIAZ</h4>
+				</a>
+			</div>
+
+			<div class="clearfix"></div>
+
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Guillermo-Rigoberto-Casola-Marcos/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/rigoartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/rigoartistred.jpg"></image>
+				<h4>GUILLERMO RIGOBERTO CASOLA MARCOS</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>artbrutartists/Roberto-OFarrill-Multan/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/robertoartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/artbrutartists/robertoartistred.jpg"></image>
+				<h4>ROBERTO O'FARRILL MULTAN</h4>
+				</a>
+			</div>
+
+			<div class="clearfix "></div>
+		</div>
 	</div>
 </div>
-
-<script type="text/javascript">
-
-	var pictures = document.querySelectorAll(".tablepicture, .tablepicturered");
-	var tds = document.querySelectorAll("tr td");
-	var spans = document.querySelectorAll(".sectiontablecontent span");
-
-
-	document.addEventListener("DOMContentLoaded", function(event){
-	
-	if(window.innerWidth>980){
-		for(var i=0; i<pictures.length; i++){
-			pictures[i].setAttribute("height", window.innerWidth*.27."px");
-			pictures[i].setAttribute("width", window.innerWidth*.27."px");
-		}
-		
-		for(var i=0; i<tds.length; i++){
-			tds[i].setAttribute("height", window.innerWidth*.27+30."px");
-			tds[i].setAttribute("width", window.innerWidth*.27+30."px");
-			
-
-		}
-
-
-	}
-	else{
-		for(var i=0; i<pictures.length; i++){
-			pictures[i].setAttribute("height", 980*.27."px");
-			pictures[i].setAttribute("width", 980*.27."px");	
-
-		}
-		for(var i=0; i<tds.length; i++){
-			tds[i].setAttribute("height", 980*.27+30."px");
-			tds[i].setAttribute("width", 980*.27+30."px");
-		}
-
-
-
-	}
-	});
-
-	window.addEventListener("resize", function(){
-	if(window.innerWidth>980){
-		for(var i=0; i<pictures.length; i++){
-			pictures[i].setAttribute("height", window.innerWidth*.27);
-			pictures[i].setAttribute("width", window.innerWidth*.27);	
-		}
-		for(var i=0; i<tds.length; i++){
-			tds[i].setAttribute("height", window.innerWidth*.27+30);
-			tds[i].setAttribute("width", window.innerWidth*.27+30);
-		}
-
-
-	}
-	else{
-		for(var i=0; i<pictures.length; i++){
-			pictures[i].setAttribute("height", 980*.27);
-			pictures[i].setAttribute("width", 980*.27);	
-
-		}
-		for(var i=0; i<tds.length; i++){
-			tds[i].setAttribute("height", 980*.27+30);
-			tds[i].setAttribute("width", 980*.27+30);
-		}
-
-
-	}
-	})
-
-</script>
--- views/outsiderartartists/index.php
+++ views/outsiderartartists/index.php
@@ -3,79 +3,77 @@
 		<div class="sectiontitle" id="artbrutartistssectiontitle" style="text-align: center; padding-top:40px;">
 			OUTSIDER ART ARTISTS
 		</div>
-		<table class="sectiontablecontent" id="artbrutartistssectiontablecontent">
-			<tbody>
-			<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Bernardo-Sarria-Almoguea/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/bernardoartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/bernardoartistred.jpg"></image>
-					<span>BERNARDO SARRIA</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Federico-Garcia-Cortizas/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/federicoartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/federicoartistred.jpg"></image>
-					<span>FEDERICO GARCIA CORTIZAS</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Isabel-Aleman-Corrales/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/isabelartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/isabelartistred.jpg"></image>
-					<span>ISABEL ALEMAN CORRALES</span>
-					</a>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Luis-M-Otero-Alcantara/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/luismanuelartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/luismanuelartistred.jpg"></image>
-					<span>LUIS MANUEL OTERO ALCANTARA</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Ramon-Moya-Hernandez/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/moyaartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/moyaartistred.jpg"></image>
-					<span>RAMON MOYA HERNANDEZ</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Pavel-Alvarez-Mesa/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/pavelartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/pavelartistred.jpg"></image>
-					<span>PAVEL ALVAREZ MESA</span>
-					</a>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Pedro-Oses-Diaz/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/pedroosesartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/pedroosesartistred.jpg"></image>
-					<span>PEDRO OSES DIAZ</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Ruben-G-Guerrero-Garrido/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/rubenartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/rubenartistred.jpg"></image>
-					<span>RUBEN GERARDO GUERRERO GARRIDO</span>
-					</a>
-				</td>
-				<td>
-					<a href="<?php echo ROOT_PATH ?>outsiderartartists/Victor-M-Moreno-Pineiro/">
-					<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/victormorenoartist.jpg"></image>
-					<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/victormorenoartistred.jpg"></image>
-					<span>VICTOR M. MORENO PINEIRO</span>
-					</a>
-				</td>
-			</tr>
-			</tbody>
-		</table>
+		<div class = "container artisttable text">
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Bernardo-Sarria-Almoguea/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/bernardoartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/bernardoartistred.jpg"></image>
+				<h4>BERNARDO SARRIA</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Federico-Garcia-Cortizas/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/federicoartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/federicoartistred.jpg"></image>
+				<h4>FEDERICO GARCIA CORTIZAS</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Isabel-Aleman-Corrales/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/isabelartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/isabelartistred.jpg"></image>
+				<h4>ISABEL ALEMAN CORRALES</h4>
+				</a>
+			</div>
+
+			<div class="clearfix"></div>
+
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Luis-M-Otero-Alcantara/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/luismanuelartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/luismanuelartistred.jpg"></image>
+				<h4>LUIS MANUEL OTERO ALCANTARA</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Ramon-Moya-Hernandez/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/moyaartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/moyaartistred.jpg"></image>
+				<h4>RAMON MOYA HERNANDEZ</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Pavel-Alvarez-Mesa/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/pavelartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/pavelartistred.jpg"></image>
+				<h4>PAVEL ALVAREZ MESA</h4>
+				</a>
+			</div>
+
+			<div class="clearfix"></div>
+
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Pedro-Oses-Diaz/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/pedroosesartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/pedroosesartistred.jpg"></image>
+				<h4>PEDRO OSES DIAZ</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Ruben-G-Guerrero-Garrido/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/rubenartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/rubenartistred.jpg"></image>
+				<h4>RUBEN GERARDO GUERRERO GARRIDO</h4>
+				</a>
+			</div>
+			<div class= "col-sm-4 col-md-4 col-lg-4">
+				<a href="<?php echo ROOT_PATH ?>outsiderartartists/Victor-M-Moreno-Pineiro/">
+				<image class="tablepicture" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/victormorenoartist.jpg"></image>
+				<image class="tablepicturered" id="artisttablepicture" src="<?php echo ROOT_PATH ?>assets/images/outsiderartartists/victormorenoartistred.jpg"></image>
+				<h4>VICTOR M. MORENO PINEIRO</h4>
+				</a>
+			</div>
+		</div>
 	</div>
 </div>
 
@@ -83,7 +81,7 @@
 
 	var pictures = document.querySelectorAll(".tablepicture, .tablepicturered");
 	var tds = document.querySelectorAll("tr td");
-	var spans = document.querySelectorAll(".sectiontablecontent span");
+	var h4s = document.querySelectorAll(".sectiontablecontent h4");
 
 
 	document.addEventListener("DOMContentLoaded", function(event){
